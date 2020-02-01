b230d3098cf0c4ba5a27de7575a08539227f66b1
Sajed Nahian <contactsajednahian@gmail.com>
2020-02-01 10:50:14 -0500

    typo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t sajednahian/code-execution-server:latest -t sajednahian/code-execution-server:$SHA -f ./code-execution-server/Dockerfile ./code-execution-server
-docker build -t sajednahian/code-runner:latest -t sajednahian/code-runner:$SHA ./code-runner/Dockerfile ./code-runner
+docker build -t sajednahian/code-runner:latest -t sajednahian/code-runner:$SHA -f ./code-runner/Dockerfile ./code-runner
 docker push sajednahian/code-execution-server:latest
 docker push sajednahian/code-execution-server:$SHA 
 docker push sajednahian/code-runner:latest
