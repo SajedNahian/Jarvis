cb8be6bd273cb818bbbfc38233cfcc7c49452e1e
Sajed Nahian <contactsajednahian@gmail.com>
2020-02-01 10:43:43 -0500

    deloy shell add code-runner

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 docker build -t sajednahian/code-execution-server:latest -t sajednahian/code-execution-server:$SHA -f ./code-execution-server/Dockerfile ./code-execution-server
-
+docker build -t sajednahian/code-runner:latest -t sajednahian/code-runner:$SHA ./code-runner/Dockerfile ./code-runner
 docker push sajednahian/code-execution-server:latest
 docker push sajednahian/code-execution-server:$SHA 
+docker push sajednahian/code-runner:latest
+docker push sajednahian/code-runner:$SHA
