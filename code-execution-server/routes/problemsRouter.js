cc1932bf78ce598f643b7d58abd53b5a7954a0e5
Sajed Nahian <contactsajednahian@gmail.com>
2020-02-05 06:35:26 -0500

    added same day support indicator

--- code-execution-server/routes/problemsRouter.js
+++ code-execution-server/routes/problemsRouter.js
@@ -9,11 +9,20 @@ const sortingOrder = {
   Hard: 3
 };
 
+const areSameDay = (d1, d2) => Math.abs(d2 - d1) <= 1000 * 60 * 60 * 24;
+
 router.get('/', async (req, res) => {
-  const problems = await Problem.find();
+  let problems = await Problem.find();
+
   problems.sort(
     (a, b) => sortingOrder[a.difficulty] - sortingOrder[b.difficulty]
   );
+
+  problems = problems.map(problem => ({
+    ...problem.toJSON(),
+    createdToday: areSameDay(problem.createdAt, Date.now())
+  }));
+
   res.json({
     problems
   });
